48a3e59c52965be25cf7227c43909d8e18076025
DmitryEskin1 <99074259+DmitryEskin1@users.noreply.github.com>
2022-12-04 19:29:10 +0300

    4

--- bin/guess-number.php
+++ bin/guess-number.php
@@ -1,12 +1,12 @@
 <?php
 
-    $autoLoadGit = __DIR__ . '/../vendor/autoload.php';
-    $autoLoadPackgaist = __DIR__ . '/../../../autoload.php';
+$autoloadGit = __DIR__ . '/../vendor/autoload.php';
+$autoloadPackagist = __DIR__ . '/../../../autoload.php';
 
-if (file_exists($autoLoadGit)) {
-    require_once($autoLoadGit);
-} else {
-    require_once($autoLoadPackgaist);
-}
-    use function dmitryeskin1\guess_number\Controller\menu;
-    menu();
+if (file_exists($autoloadGit))
+    require_once($autoloadGit);
+else
+    require_once($autoloadPackagist);
+
+use function dmitryeskin1\guess_number\Controller\startGame;
+startGame();
